79f3339dcf1e29ac3f305dd0d5a560f8d3d77122
banama <306672962@qq.com>
2015-04-09 13:49:22 +0800

    arrange && uncss

--- generator-feyo/appgulp/appgulp.js
+++ generator-feyo/appgulp/appgulp.js
@@ -4,41 +4,64 @@ var jshint = require('./jshint')
 var browserify = require('./browserify')
 var scss = require('./scss')
 var css = require('./css')
+var uncss = require('./uncss')
 var hash = require('./hash')
 var clean = require('./clean')
 var rev = require('./rev').revF
 
 
-
-// Base
-gulp.task('cssbase', function(){
+// css
+gulp.task('scss', function(){
     scss.scssF('../scss/*.scss', '../css')
-    // clean.cleanF('../dist/css')
-    css.cssF('../css/*.css', '../dist/css')
-    hash.hashcssF('../dist/css/*.css', '../dist/css/hash/', '../dist/css')
 })
 
-gulp.task('jsbase', function(){
-    jshint.jshintF('../js/*.js')
-    // clean.cleanF('../dist/js')
-    browserify.browserifyF('../js/app.js', '../dist/js')
-    hash.hashjsF('../dist/js/*.js', '../dist/js/hash/', '../dist/js')       
+gulp.task('uncss', function(){
+    uncss.uncssF('../css/*.css', ['index.html'], '../css')
+})
+
+gulp.task('cssmin', function(){
+    css.cssF('../css/*.css', '../dist/css')
+})
+
+gulp.task('csshash', function(){
+    // clean.cleanF('../dist/css')
+    hash.hashcssF('../dist/css/*.css', '../dist/css/hash/', '../dist/css')
 })
 
 gulp.task('revcss', function(){
     rev('../dist/css/rev-manifest.json', '../jade/index.html', '../jade')
 })
 
+
+// js
+gulp.task('jshint', function(){
+    jshint.jshintF('../js/*.js')
+})
+
+gulp.task('browserify', function(){
+    browserify.browserifyF('app.bundle.js', '../js/app.js', '../dist/js')
+})
+
+gulp.task('jshash', function(){
+    // clean.cleanF('../dist/js')
+    hash.hashjsF('../dist/js/*.js', '../dist/js/hash/', '../dist/js')       
+})
+
 gulp.task('revjs', function(){
     rev('../dist/js/rev-manifest.json', '../jade/index.html', '../jade')
 })
 
-gulp.task('cleanbase', function(){
+gulp.task('clean', function(){
     clean.cleanF('../dist/')
 })
 
 gulp.task('run', function(){
     gulp.watch(['../scss/*.scss', '../css/*.css', '../css/*.css'], ['cssbase', 'revcss'])
     gulp.watch(['../js/*.js', '../js/app.js', '../dist/js/*.js'], ['jsbase', 'revjs'])
+})
+
+// test
+gulp.task('uncss', function(){
+    uncss.uncssF('./test/css.css', ['index.html'], './test')
 })
 
